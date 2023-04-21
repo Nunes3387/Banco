464863c921941146f0a60f1eaddbd79af6659bc5
NuNes <NuNes@DESKTOP-RKLAV38>
2023-04-21 17:26:03 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp8/ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class ContaBancaria : IContaBancaria
+    {
+        private decimal saldo;
+        private int numero;
+
+        public ContaBancaria(int numero)
+        {
+            this.numero = numero;
+            this.saldo = 0;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            saldo += valor;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+            }
+            else
+            {
+                throw new Exception("Saldo insuficiente");
+            }
+        }
+
+        public decimal ConsultarSaldo()
+        {
+            return saldo;
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria(1111);
+
+            decimal n1, n2;
+
+            Console.WriteLine("Digite o valor do Deposito:");
+            n1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do Saque:");
+            n2 = decimal.Parse(Console.ReadLine());
+
+            conta.Depositar(n1);
+            conta.Sacar(n2);
+            decimal saldo = conta.ConsultarSaldo();
+            Console.WriteLine("Saldo atual: " + saldo); 
+
+            Console.ReadKey();
+
+        }
+    }
+}
